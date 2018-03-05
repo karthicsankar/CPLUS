6e0b845fd4909b83e8b28b70868168b435bbb504
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-03-05 16:44:23 +0530

    swapshifft.C

---
+++ swapshifft.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main() 
+{	
+   char input[10],output[10];
+   int length;
+   printf("Enter the string:");
+   scanf("%s",input);
+   length=strlen(input);
+   for( int i=0;i<length;i=i+2)
+   {
+      output[i]=input[i];
+      printf("%c",output[i]);
+   }
+   printf(" ");
+   for(int i=1;i<length;i=i+2)
+   {
+      output[i]=input[i];
+      printf("%c",output[i]);
+   }
+ return 0;
+}
