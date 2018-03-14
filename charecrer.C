55489cadbe8bc86bdaf1e36045e894f0fbeac1aa
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-03-14 15:38:36 +0530

    charecrer.C

---
+++ charecrer.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int number;
+printf("Enter a number:");
+scanf("%d",&number);
+printf("The  Equivalent  character is:%c",number+96);
+getch();
+return 0;
+}
