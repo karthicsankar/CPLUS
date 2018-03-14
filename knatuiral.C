0cf0bc029a2ab529753c7c5e4c10f79117906397
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-03-14 15:59:30 +0530

    knatuiral.C

--- knatuiral.C
+++ knatuiral.C
@@ -1,9 +1,9 @@
 #include<stdio.h>
 void main()
 {
-int Number,i,a;
+int number,i,a;
 printf("Enter a Number : ");
-scanf("%d",&Number);             //getting the number
-a=(Number*(Number+1))/2;          //arithmatic progression
+scanf("%d",&number);             //getting the number
+a=(number*(number+1))/2;          //arithmatic progression
 printf("%d",a);
 }
