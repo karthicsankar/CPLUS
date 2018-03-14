ea39c612011a8b722b19e63db6cb963c687b0cb2
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-03-14 16:26:44 +0530

    smallestofarray.C

---
+++ smallestofarray.C
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void) {
+	int number,m,i,j,a[10];
+	printf("Enter the number of array:");
+	scanf("%d %d",&number,&m);
+	for(i=0;i<number;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<number;i++)
+	{
+		if(a[i]%2!=0)
+		{
+			
+			for(j=i+1;j<number;j++)
+			{
+				if(a[j]<a[j+1])
+				{
+					printf("%d",a[j]);
+					break;
+				}
+			}
+		break;
+		}
+		
+	}
+	
+	return 0;
+}
